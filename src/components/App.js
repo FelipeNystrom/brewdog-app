f4cd878e4ce05a71b4ef5c567c8b29ebd0a666bc
FelipeNystrom <andre.felipe.nystrom@gmail.com>
2018-09-07 13:29:29 +0200

    add toggle view functionality based on user

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
-import React, { Component } from "react";
-import StartPage from "./StartPage";
-import Navbar from "./Navbar";
+import React, { Component } from 'react';
+import StartPage from './StartPage';
+import Navbar from './Navbar';
+import Favorites from './Favorites';
 
 class App extends Component {
+  state = {
+    showDefault: true,
+    showFavorites: false
+  };
+
+  // toggle state to control which component is shown
+  toogleView = isLoggedIn => {
+    if (isLoggedIn) {
+      this.setState({
+        showDefault: !this.state.showDefault,
+        showFavorites: !this.state.showFavorites
+      });
+    }
+  };
+
   render() {
+    const { showDefault, showFavorites } = this.state;
     return (
       <div>
-        <Navbar />
-        <StartPage />
+        <Navbar toggleView={this.toogleView} />
+        {showDefault && <StartPage />}
+        {showFavorites && <Favorites />}
       </div>
     );
   }
