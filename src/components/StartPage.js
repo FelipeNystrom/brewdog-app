e911690e653fd17295c3abcc8b26343749f28aa8
dmalvi <dmalvi@gmail.com>
2018-09-11 10:05:05 +0200

    styling of startpage and categories comp2

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -1,18 +1,12 @@
 import React, { Component, Fragment } from 'react';
-<<<<<<< HEAD
 import Categories from './Categories.js';
-import './StartPage.css';
-
-=======
-import { Link } from 'react-router-dom';
->>>>>>> f9c7c3b11b03dbb3d073235f1a8c5759738765d3
 
 //states
 class StartPage extends Component {
   state = {
-    meal: '',
+    meal: "",
     choiceIsMade: false,
-    choice: '',
+    choice: "",
     hasChoice: false
   };
 
@@ -31,106 +25,71 @@ class StartPage extends Component {
     });
   };
 
+  goBack = () => {
+    this.setState({ choiceIsMade: false });
+  };
+
   //return either start-page or filter-page
   render() {
-<<<<<<< HEAD
     const { meal, choiceIsMade } = this.state;
     const dinnerChoices = ["Chicken", "Beef", "Pork", "Lamb", "Fish"];
     const dessertChoices = ["Chocolate", "Ice Cream", "Cheesecake", "Cookies"];
 
     const listDinners = dinnerChoices.map((dinner, i) => (
-      <div
+      <button
         key={i}
         onClick={this.handleCategory}
         name="choice"
         value={dinner}
-        className="chooseIngredient"
       >
         {dinner}
-      </div>
+      </button>
     ));
 
     const listDesserts = dessertChoices.map((dessert, i) => (
-      <div
+      <button
         key={i}
         onClick={this.handleCategory}
         name="choice"
         value={dessert}
-        className="chooseIngredient"
       >
         {dessert}
-      </div>
+      </button>
     ));
     return (
       <Fragment>
         {!choiceIsMade ? (
-          <div className="mainContainer">
-            <div className="textContainer">
-              <h1>Beerit</h1>
-              <h3>Match food with beer</h3>
-              <h4>Choose meal</h4>
-            </div>
-            <div className="childDiv">
-                <button
-=======
-    return (
-      <Fragment>
-        <div>
-          <h1>Beerit</h1>
-          <h3>Match food with beer</h3>
-          <h4>Choose meal</h4>
           <div>
-            <Link to={`/categories/dinner`}>
+            <h1>Beerit</h1>
+            <h3>Match food with beer</h3>
+            <h4>Choose meal</h4>
+            <div>
               <button
->>>>>>> f9c7c3b11b03dbb3d073235f1a8c5759738765d3
                 type="button"
                 onClick={this.handleClick}
                 name="meal"
                 value="dinner"
-                className="choiceButton dinner"
-                >
+              >
                 Dinner
               </button>
-<<<<<<< HEAD
-              </div>
-              <div className="childDiv">
-                <button
-=======
-            </Link>
-            <Link to={`/categories/dessert`}>
               <button
->>>>>>> f9c7c3b11b03dbb3d073235f1a8c5759738765d3
                 type="button"
                 onClick={this.handleClick}
                 name="meal"
                 value="dessert"
-                className="choiceButton dessert"
-                >
+              >
                 Dessert
               </button>
-<<<<<<< HEAD
-              </div>
+            </div>
           </div>
         ) : (
           <Categories state={this.state}>
-            <div className="mainContainer">
-              <div className="textContainer">
-                <h2>Choose main ingredient </h2>
-              </div>
-              <div className="childDiv">
-              {meal === "dinner" ? listDinners : listDesserts}
-              </div>
-              <div>
-              <button className="chooseIngredient" onClick={this.goBack}>Back</button>
-              </div>
+            {meal === "dinner" ? listDinners : listDesserts}
+            <div>
+              <button onClick={this.goBack}>Back</button>
             </div>
           </Categories>
         )}
-=======
-            </Link>
-          </div>
-        </div>
->>>>>>> f9c7c3b11b03dbb3d073235f1a8c5759738765d3
       </Fragment>
     );
   }
