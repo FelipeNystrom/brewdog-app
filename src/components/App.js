8431b081fa7044c0c95e7e1e0d09d87d94373176
Marie Flem H <marie_fh@hotmail.com>
2018-09-11 11:21:00 +0200

    changed container div to app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
 // Components to route
-import StartPage from './StartPage';
-import Navbar from './Navbar';
-import Favorites from './Favorites';
-import Categories from './Categories.js';
-import DisplayCards from './DisplayCards';
+import StartPage from "./StartPage";
+import Navbar from "./Navbar";
+import Favorites from "./Favorites";
+import Categories from "./Categories.js";
+import DisplayCards from "./DisplayCards";
 
 class App extends Component {
   render() {
@@ -28,7 +28,11 @@ class App extends Component {
         />
         <Route
           path="/beers-to-match-with/:foodTypeToMatchWith"
-          render={props => <DisplayCards {...props} />}
+          render={props => (
+            <div className="container">
+              <DisplayCards {...props} />
+            </div>
+          )}
         />
       </div>
     );
